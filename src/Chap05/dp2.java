1480abecf3582754994b57b8262a0b1f0bd9eb67
PJY <parkjy0629@gmail.com>
2021-11-07 01:39:11 +0900

    20211107_Chap21,22

---
+++ src/Chap05/dp2.java
@@ -0,0 +1,26 @@
+package Chap05;
+
+// 2 x n 타일링 - 11726
+
+import java.util.Scanner;
+
+public class dp2 {
+    static int[] d = new int[1001];
+
+    static int dp(int x) {
+        if (x == 1) return 1;
+        if (x == 2) return 2;
+
+        if (d[x] != 0) return d[x];
+
+        return d[x] = (dp(x -1) + dp(x - 2)) % 10007;
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        int n = stdIn.nextInt();
+
+        System.out.println(dp(n));
+    }
+}
